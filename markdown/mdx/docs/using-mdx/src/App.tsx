0a7c007583fd9febaf2f359d0dd785f4bb26c3fa
gopaladhikari <gopuadks@gmail.com>
2024-05-23 19:57:51 +0545

    markdown/mdx --> using mdx in react with mdx rollup

---
+++ markdown/mdx/docs/using-mdx/src/App.tsx
@@ -0,0 +1,11 @@
+import Welcome from "./content/welcome.mdx";
+import "./App.css";
+
+export default function App() {
+	return (
+		<main>
+			<h1>Welcome to MDX</h1>
+			<Welcome />
+		</main>
+	);
+}
