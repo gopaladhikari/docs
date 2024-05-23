64804e45f4fb81e7b1470c175e588533fc06db32
gopaladhikari <gopuadks@gmail.com>
2024-05-23 21:53:26 +0545

    setup tailwindcss

--- markdown/mdx/docs/using-mdx/src/App.tsx
+++ markdown/mdx/docs/using-mdx/src/App.tsx
@@ -1,11 +1,5 @@
 import Welcome from "./content/welcome.mdx";
-import "./App.css";
 
 export default function App() {
-	return (
-		<main>
-			<h1>Welcome to MDX</h1>
-			<Welcome />
-		</main>
-	);
+	return <Welcome />;
 }
