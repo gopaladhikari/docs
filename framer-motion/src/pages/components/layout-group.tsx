0d4388a1cf6bff7b48c508b30edcfccd0f82c5c3
gopaladhikari <gopuadks@gmail.com>
2024-05-30 17:31:42 +0545

    layout group

--- framer-motion/src/pages/components/layout-group.tsx
+++ framer-motion/src/pages/components/layout-group.tsx
@@ -1,3 +1,37 @@
+import { LayoutGroup as LG, motion } from "framer-motion";
+import { useState } from "react";
+
+interface Props {
+  header: string;
+  content: string;
+}
+
+function ToggleContent({ header, content }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <motion.div layout onClick={() => setIsOpen(!isOpen)}>
+      <motion.h2
+        exit={{
+          opacity: 0,
+        }}
+        layout
+      >
+        {header}
+      </motion.h2>
+      {isOpen ? content : null}
+    </motion.div>
+  );
+}
+
 export function LayoutGroup() {
-	return <div> gopaladhikari </div>;
+  return (
+    <LG>
+      <ToggleContent header="Header" content="This is content" />
+      <ToggleContent
+        header="This is from another"
+        content="Jinjalala hurhur"
+      />{" "}
+    </LG>
+  );
 }
