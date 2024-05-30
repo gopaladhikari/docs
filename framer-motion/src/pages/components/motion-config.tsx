72e125a19388b6417b4716ebbdd9f79b0ea6d0e3
gopaladhikari <gopuadks@gmail.com>
2024-05-30 17:42:21 +0545

    motion config

--- framer-motion/src/pages/components/motion-config.tsx
+++ framer-motion/src/pages/components/motion-config.tsx
@@ -1,3 +1,17 @@
+import { motion, MotionConfig as MC } from "framer-motion";
+
 export function MotionConfig() {
-	return <div> gopaladhikari </div>;
+  return (
+    <div>
+      <h1>MotionConfig</h1>
+      <p>Set configuration options for Framer Motion.</p>
+
+      <MC transition={{ duration: 1 }}>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
+          accusamus.
+        </motion.div>
+      </MC>
+    </div>
+  );
 }
