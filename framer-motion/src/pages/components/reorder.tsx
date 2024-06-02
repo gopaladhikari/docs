9ec79889e9f32fc4b5bbddb5458a9a55ce0f80cf
gopaladhikari <gopuadks@gmail.com>
2024-06-02 09:53:00 +0545

    reorder

--- framer-motion/src/pages/components/reorder.tsx
+++ framer-motion/src/pages/components/reorder.tsx
@@ -1,3 +1,46 @@
+import {
+  AnimatePresence,
+  Reorder as Re,
+  useDragControls,
+} from "framer-motion";
+import { useState } from "react";
+
 export function Reorder() {
-	return <div> reorder </div>;
+  const [items, setItems] = useState([0, 1, 2, 3]);
+
+  const controls = useDragControls();
+
+  return (
+    <div>
+      <h1>Reorder</h1>{" "}
+      <p>
+        Create drag-to-reorder effects with a simple set of components.
+      </p>
+      <Re.Group
+        axis="x"
+        values={items}
+        onReorder={setItems}
+        className="flex gap-6"
+        as="div"
+      >
+        <AnimatePresence>
+          {items.map((item) => (
+            <Re.Item
+              dragControls={controls}
+              key={item}
+              value={item}
+              as="div"
+            >
+              <div
+                onPointerDown={(e) => controls.start(e)}
+                className="bg-red-400 h-20 w-20 rounded-full flex justify-center items-center"
+              >
+                {item}
+              </div>
+            </Re.Item>
+          ))}
+        </AnimatePresence>
+      </Re.Group>
+    </div>
+  );
 }
