fc137ceab2d7dfbd5907e3516f5c312ae126e755
gopaladhikari <gopuadks@gmail.com>
2024-06-11 08:59:13 +0545

    motion value overview

--- framer-motion/src/pages/motion-values/overview.tsx
+++ framer-motion/src/pages/motion-values/overview.tsx
@@ -1,3 +1,32 @@
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
+
 export default function MotionOverview() {
-  return <div>Overview</div>;
+  const x = useMotionValue(119);
+
+  useEffect(() => {
+    x.set(100);
+  }, []);
+
+  return (
+    <main>
+      <h1>Motion values overview</h1>
+      <p>
+        Motion values track the state and velocity of animating values.
+      </p>
+
+      <p>
+        Get value is
+        {x.get()} {x.getVelocity()}
+      </p>
+
+      <motion.circle
+        className="flex items-center justify-center gap-2 p-4 bg-slate-800 rounded-lg"
+        cx={x}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
+      >
+        {x}
+      </motion.circle>
+    </main>
+  );
 }
