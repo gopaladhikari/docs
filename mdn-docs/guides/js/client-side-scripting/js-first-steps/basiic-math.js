0d99abe2708d3a795efc10ddecbe86101f839ab7
gopaladhikari <gopuadks@gmail.com>
2024-10-24 10:46:37 +0545

    basic math in  js

---
+++ mdn-docs/guides/js/client-side-scripting/js-first-steps/basiic-math.js
@@ -0,0 +1,12 @@
+const lotsOfDecimal = 1.766584958675746364;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+
+let num2 = 6;
+console.log(num2);
+num2--;
+console.log(num2);
+num2++;
+console.log(num2);
