9167d2b16d3cefaf738b7a274a8437043876c7cf
gopaladhikari <gopuadks@gmail.com>
2024-10-24 10:53:45 +0545

    handling text in js

---
+++ mdn-docs/guides/js/client-side-scripting/js-first-steps/handling-text.js
@@ -0,0 +1,13 @@
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const badString = string;
+console.log(badString);
+
+const newline = `One day you finally knew
+what you had to do, and began,`;
+console.log(newline);
+
+const newline2 =
+  "One day you finally knew\nwhat you had to do, and began,";
+console.log(newline2);
