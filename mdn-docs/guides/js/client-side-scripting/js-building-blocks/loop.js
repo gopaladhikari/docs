a0479ea0a65b7a0ee180d8d6ea84b3f533b0167d
gopaladhikari <gopuadks@gmail.com>
2024-11-19 12:18:45 +0545

    loops

---
+++ mdn-docs/guides/js/client-side-scripting/js-building-blocks/loop.js
@@ -0,0 +1,23 @@
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
+// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
+
+function lCat(cat) {
+  return cat.startsWith("L");
+}
+
+const filtered = cats.filter(lCat);
+
+console.log(filtered);
+// [ "Leopard", "Lion" ]
